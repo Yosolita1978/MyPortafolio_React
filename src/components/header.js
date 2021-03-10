0e16da18e88d6bda02a2d477f7f4390480b399bd
Cristina Rodriguez <cristina@Cristinas-MacBook-Pro.local>
2021-03-10 14:55:31 -0800

    The site is responsive now

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import { Header, Navigation } from 'react-mdl';
+import { Header, Navigation, Drawer, Layout } from 'react-mdl';
 import { Link } from "react-router-dom";
 
 
 class HeaderNav extends Component{
     render(){
       return (
-        <div >
+        <div className="demo-big-content" >
+          <Layout>
         <Header className="header-color" title={<Link style={{textDecoration: 'none', color: 'white'}} to="/">Cristina Rodriguez</Link>} scroll>
               <Navigation>
                   <Link to="/aboutme">About Me</Link>
@@ -14,7 +15,17 @@ class HeaderNav extends Component{
                   <a href="https://github.com/Yosolita1978/MyPortafolio_React/blob/main/public/CristinaCV2021.pdf?raw=true">Resume</a>
                   <a href="mailto:yosola@gmail.com">Contact Me</a>
                 </Navigation>
-          </Header>
+                </Header>
+                <Drawer title={<Link style={{textDecoration: 'none', color: 'black'}} to="/">Cristina Rodriguez</Link>}>
+            <Navigation>
+            <Link to="/aboutme">About Me</Link>
+            <Link to="/projects">Projects</Link>
+            <a href="https://github.com/Yosolita1978/MyPortafolio_React/blob/main/public/CristinaCV2021.pdf?raw=true">Resume</a>
+            <a href="mailto:yosola@gmail.com">Contact Me</a>
+            </Navigation>
+        </Drawer>
+          
+          </Layout>
       </div>
       );
     }
